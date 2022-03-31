0b34906afd629490bd6aa34bd9bd71f83ebbbcae
Pham Huy Thien <thiendz.dev@gmail.com>
2022-03-31 10:10:11 +0700

    ok

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const crypto = require("crypto-js");
 let sha256 = crypto.SHA256;
 
 let sha = sha256("PhamHuyThien").toString(crypto.enc.Hex);
-console.log(`PhamHuyThien sha256 = ${sha}.`);
+console.log(`PhamHuyThien sha256 = ${sha}, env=${process.env.NODE_ENV}`);
